24597d8dc5897181b7f94577b354711730a9b632
Fatima Rawoot <fatimarawoot86@gmail.com>
2026-08-22 22:46:53 +0300

    JavaScript Operators

---
+++ JavaScript/Day13/marks.js
@@ -0,0 +1,19 @@
+let marks = 82;
+if(marks >= 90 && marks <= 100){
+    console.log("A");
+}
+else if (marks >= 75 && marks <= 89){
+    console.log("B");
+}
+else if (marks >= 60 && marks <= 74){
+    console.log("C");
+}
+else if (marks >= 40 && marks <= 59){
+    console.log("D");
+}
+else if (marks < 40){
+    console.log("F");
+}
+else{
+    console.log("Invalid")
+}
