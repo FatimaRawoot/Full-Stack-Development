962554d4b20680403369825d57dda7f4c1627e79
Fatima Rawoot <fatimarawoot86@gmail.com>
2026-08-23 23:09:24 +0300

    Added JavaScript functions and arrow function practice

---
+++ JavaScript/Day14/evenOdd.js
@@ -0,0 +1,9 @@
+function evenOdd(a){
+    if(a%2 == 0){
+        console.log("Even no.");
+    }
+    else{
+        console.log("Odd no.");
+    }
+}
+evenOdd(7);
---
+++ JavaScript/Day14/sum.js
@@ -0,0 +1,5 @@
+const sum = (a, b) => {
+    return a+b;
+}
+let add = sum(4, 2);
+console.log(add);
