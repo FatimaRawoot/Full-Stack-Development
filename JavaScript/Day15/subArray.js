6fed64336417f5189307c4b8e1c718877dcbdf0e
Fatima Rawoot <fatimarawoot86@gmail.com>
2026-08-24 23:29:30 +0300

    JavaScript array concept - length, index, array element update

---
+++ JavaScript/Day15/subArray.js
@@ -0,0 +1,7 @@
+let subjects = ["HTML", "CSS", "JavaScript", "DSA"];
+//print JavaScript
+console.log(subjects[2]);
+//print DSA
+console.log(subjects[3]);
+//length of array
+console.log(subjects.length);
---
+++ JavaScript/Day15/updateArray.js
@@ -0,0 +1,3 @@
+let Skills = ["HTML", "CSS", "JavaScript"];
+Skills[1] = "react";
+console.log(Skills);
