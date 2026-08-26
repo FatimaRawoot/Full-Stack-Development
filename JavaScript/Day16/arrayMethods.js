898469675481ec8ac5c755cb7a2a2924fccd9780
Fatima Rawoot <fatimarawoot86@gmail.com>
2026-08-26 22:00:56 +0300

    JavaScript Array Methods and Concat

---
+++ JavaScript/Day16/arrayMethods.js
@@ -0,0 +1,10 @@
+let fruits = ["Apple", "Mango"];
+//Add banana at the end
+fruits.push("Banana");
+//Add orange at the beginning
+fruits.unshift("Orange");
+//Remove last element
+fruits.pop();
+//Remove first element
+fruits.shift();
+console.log(fruits);
---
+++ JavaScript/Day16/combineArrays.js
@@ -0,0 +1,5 @@
+let frontend = ["HTML", "CSS", "JavaScript"];
+let backend = ["Node.js", "Express"];
+//Combine both the array
+let fullStack = frontend.concat(backend);
+console.log(fullStack);
