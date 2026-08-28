4d318c00adefd284b1d5a08774652c37b0232415
Fatima Rawoot <fatimarawoot86@gmail.com>
2026-08-28 22:21:31 +0300

    Learned JavaScript loops

---
+++ JavaScript/Day17/conditionInLoop.js
@@ -0,0 +1,6 @@
+let numbers = [12, 5, 8, 21, 4, 17];
+for(let i = 0; i < numbers.length; i++){
+    if(numbers[i]>10){
+        console.log("Numbers is greater than 10 : ", numbers[i]);
+    }
+}
---
+++ JavaScript/Day17/sumOfArray.js
@@ -0,0 +1,8 @@
+let marks = [10, 20, 30, 40, 50];
+let sum = 0;
+let i =0;
+while(i < marks.length){
+    sum = sum + marks[i];
+    i++;
+}
+console.log(sum);
