bbf2a86dcc972132bad3644f3373772f96bca223
Fatima Rawoot <fatimarawoot86@gmail.com>
2026-09-19 23:26:01 +0530

    Day 31 - JavaScript Functions Deep Dive

---
+++ JavaScript/Day31/function.js
@@ -0,0 +1,9 @@
+function welcome(){
+    console.log("Welcome to Full Stack Development!");
+}
+function studentInfo(){
+    console.log("I am learning JavaScript.");
+}
+
+welcome();
+studentInfo();
