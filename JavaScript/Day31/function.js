a6b43f24485f7a4d6b77c6072952a4c2a4c41c79
Fatima Rawoot <fatimarawoot86@gmail.com>
2026-09-20 10:44:14 +0530

    Day 31 - Practice Parameters and Arguments

--- JavaScript/Day31/function.js
+++ JavaScript/Day31/function.js
@@ -4,6 +4,19 @@ function welcome(){
 function studentInfo(){
     console.log("I am learning JavaScript.");
 }
+function greetUser(name){
+    console.log("Hello " + name);
+}
+function square(num){
+    let numSquare = num * num;
+    console.log(numSquare);
+}
+function calculateAge(name, age){
+    console.log(name + " is " + age + " years old");
+}
 
 welcome();
 studentInfo();
+greetUser("Fatima");
+square(5);
+calculateAge("Fatima", 21);
