d16f44088aa3fef36eb39fafc40a67b5e136012d
Fatima Rawoot <fatimarawoot86@gmail.com>
2026-09-25 22:00:33 +0530

    Day 32 - JavaScript Closures

---
+++ JavaScript/Day32/closure.js
@@ -0,0 +1,14 @@
+function createCounter(){
+    let count = 0;
+    function inner(){
+        count++;
+        console.log(count);
+    }
+    return inner;
+}
+let counter = createCounter();
+counter();
+counter();
+counter();
+counter();
+counter();
